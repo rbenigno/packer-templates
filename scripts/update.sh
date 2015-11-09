d53fb04900b631607b4e69a4966f92f094f425c1
Ryan Benigno <ryanb@pobox.com>
2015-11-09 10:11:13 -0800

    Move CentOS update to provisioner

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,28 @@
 #!/bin/bash -eux
 
-apt-get update
-# apt-get install -qy open-vm-tools
-apt-get -qy dist-upgrade
+if [ -f /etc/os-release ]
+then
+        . /etc/os-release
+else
+        echo "ERROR: /etc/os-release not present"
+        exit
+fi
 
-# Hmm, reboot-required flag not always set after kernel update?
-#if [ -f /var/run/reboot-required ]; then
-	reboot
-	sleep 60
-#fi
+case $NAME in
+    Ubuntu)
+        apt-get update
+        # apt-get install -qy open-vm-tools
+        apt-get -qy dist-upgrade
+
+        # Hmm, reboot-required flag not always set after kernel update?
+        #if [ -f /var/run/reboot-required ]; then
+            reboot
+            sleep 60
+        #fi
+        ;;
+    CentOS*)
+        # Install Updates
+        yum update -y
+        yum clean all
+        ;;
+esac
